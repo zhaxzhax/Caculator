bbeb34fa9cd3e62759975ebee40e290ba3923e9d
zhaxzhax <707627402@qq.com>
2019-07-17 18:24:13 +0800

    something have not commited to the project!

--- src/main/java/com/foy/caculator/MainActivity.java
+++ src/main/java/com/foy/caculator/MainActivity.java
@@ -2,12 +2,159 @@ package com.foy.caculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViewById(R.id.leftpa).setOnClickListener(this);
+        findViewById(R.id.rightpa).setOnClickListener(this);
+        findViewById(R.id.back).setOnClickListener(this);
+        findViewById(R.id.division).setOnClickListener(this);
+        findViewById(R.id.sqrt).setOnClickListener(this);
+        findViewById(R.id.multiply).setOnClickListener(this);
+        findViewById(R.id.one).setOnClickListener(this);
+        findViewById(R.id.two).setOnClickListener(this);
+        findViewById(R.id.three).setOnClickListener(this);
+        findViewById(R.id.four).setOnClickListener(this);
+        findViewById(R.id.five).setOnClickListener(this);
+        findViewById(R.id.six).setOnClickListener(this);
+        findViewById(R.id.seven).setOnClickListener(this);
+        findViewById(R.id.eight).setOnClickListener(this);
+        findViewById(R.id.nine).setOnClickListener(this);
+        findViewById(R.id.zero).setOnClickListener(this);
+        findViewById(R.id.dot).setOnClickListener(this);
+        findViewById(R.id.delete).setOnClickListener(this);
+        findViewById(R.id.add).setOnClickListener(this);
+        findViewById(R.id.substract).setOnClickListener(this);
+        findViewById(R.id.equal).setOnClickListener(this);
+
+    }
+    @Override
+    public void onClick(View v){
+        TextView equation = findViewById(R.id.equation);
+        TextView result = findViewById(R.id.result);
+        String equation_s = equation.getText().toString();
+        if (v.getId()==R.id.leftpa)
+        {
+            String newStr = String.format("%s%s",equation_s,"(");
+            equation.setText(newStr);
+        }
+        else  if (v.getId()==R.id.rightpa)
+        {
+            String newStr = String.format("%s%s",equation_s,")");
+            equation.setText(newStr);
+        }
+        else  if (v.getId()==R.id.back)
+        {
+            String newStr = equation_s.substring(0,equation_s.length()-1);
+            equation.setText(newStr);
+        }
+        else  if (v.getId()==R.id.division)
+        {
+            String newStr = String.format("%s%s",equation_s,"÷");
+            equation.setText(newStr);
+        }
+        else  if (v.getId()==R.id.sqrt)
+        {
+            String newStr = String.format("%s%s",equation_s,"√");
+            equation.setText(newStr);
+        }
+        else  if (v.getId()==R.id.multiply)
+        {
+            String newStr = String.format("%s%s",equation_s,"X");
+            equation.setText(newStr);
+        }
+        else  if (v.getId()==R.id.one)
+        {
+            String newStr = String.format("%s%s",equation_s,"1");
+            equation.setText(newStr);
+        }
+        else  if (v.getId()==R.id.two)
+        {
+            String newStr = String.format("%s%s",equation_s,"2");
+            equation.setText(newStr);
+        }
+        else  if (v.getId()==R.id.three)
+        {
+            String newStr = String.format("%s%s",equation_s,"e");
+            equation.setText(newStr);
+        }
+        else  if (v.getId()==R.id.four)
+        {
+            String newStr = String.format("%s%s",equation_s,"4");
+            equation.setText(newStr);
+        }
+        else  if (v.getId()==R.id.five)
+        {
+            String newStr = String.format("%s%s",equation_s,"5");
+            equation.setText(newStr);
+        }
+        else  if (v.getId()==R.id.six)
+        {
+            String newStr = String.format("%s%s",equation_s,"6");
+            equation.setText(newStr);
+        }
+        else  if (v.getId()==R.id.seven)
+        {
+            String newStr = String.format("%s%s",equation_s,"7");
+            equation.setText(newStr);
+        }
+        else  if (v.getId()==R.id.eight)
+        {
+            String newStr = String.format("%s%s",equation_s,"8");
+            equation.setText(newStr);
+        }
+        else  if (v.getId()==R.id.nine)
+        {
+            String newStr = String.format("%s%s",equation_s,"9");
+            equation.setText(newStr);
+        }
+        else  if (v.getId()==R.id.add)
+        {
+            String newStr = String.format("%s%s",equation_s,"+");
+            equation.setText(newStr);
+        }
+        else  if (v.getId()==R.id.substract)
+        {
+            String newStr = String.format("%s%s",equation_s,"-");
+            equation.setText(newStr);
+        }
+        else  if (v.getId()==R.id.delete)
+        {
+            String newStr = "";
+            result.setText(newStr);
+            equation.setText(newStr);
+        }
+        else  if (v.getId()==R.id.dot)
+        {
+            String newStr = String.format("%s%s",equation_s,".");
+            equation.setText(newStr);
+        }
+        else  if (v.getId()==R.id.zero)
+        {
+            String newStr = String.format("%s%s",equation_s,"0");
+            equation.setText(newStr);
+        }
+        else if (v.getId()==R.id.equal)
+        {
+            String newStr;
+            if (equation_s.charAt(0)=='-'){ newStr = String.format("%s%s","0",equation_s);}
+            else { newStr = equation_s;}
+           try{
+                String finres;
+                double res = caculate.parse(newStr);
+                if (String.valueOf(res)=="Infinity")finres="除数不能为0";
+                else finres=String.valueOf(res);
+                result.setText(finres);
+            }
+           catch (Exception e) {
+               result.setText("输入有误无法计算！");
+           }
+        }
     }
 }
---
+++ src/main/java/com/foy/caculator/caculate.java
@@ -0,0 +1,50 @@
+package com.foy.caculator;
+
+public class caculate {
+    /**
+     * 实现混合运算
+     *
+     * @param content
+     * @return
+     */
+    public static double parse(String content) {
+
+        // 找到字符串中最后一个左括号
+        int startIndex = content.lastIndexOf("(");
+        // 如果不是-1,标识这个等式中有括号,继续找与之对应的右括号
+        if (startIndex != -1) {
+            // 从左括号的位置开始找,找到第一个右括号,这对括号里面一定没有括号,所以就可以交给写好的parse(Stringcontent)方法算出结果！
+            int endIndex = content.indexOf(")", startIndex);
+            double d = parse(content.substring(startIndex + 1, endIndex));
+            return parse(content.substring(0, startIndex) //
+                    + d + content.substring(endIndex + 1));
+        }
+
+        int index = content.indexOf("+");
+        if (index != -1) {
+            return parse(content.substring(0, index)) + parse(content.substring(index + 1));
+        }
+        // 这里<---
+        index = content.lastIndexOf("-");
+        if (index != -1&&index !=0&&content.charAt(index-1)!='X'&&content.charAt(index-1)!='÷'&&content.charAt(index-1)!='√'&&content.charAt(index-1)!='-') {
+            return parse(content.substring(0, index)) - parse(content.substring(index + 1));
+        }
+        // 这里<---
+        index = content.indexOf("X");
+        if (index != -1) {
+            return parse(content.substring(0, index)) * parse(content.substring(index + 1));
+        }
+        // 这里<---
+        index = content.lastIndexOf("÷");
+        if (index != -1) {
+            return parse(content.substring(0, index)) / parse(content.substring(index + 1));
+        }
+        // 这里<---
+        index = content.indexOf("√");
+        if (index != -1) {
+            return Math.sqrt(parse(content.substring(index + 1)));
+        }
+
+        return Double.parseDouble(content);
+    }
+}
